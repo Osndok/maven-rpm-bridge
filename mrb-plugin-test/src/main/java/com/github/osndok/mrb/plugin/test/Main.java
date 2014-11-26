db6bce9b0452fd1722b702691a1e5390d1725996
Robert Hailey <rhailey@allogy.com>
2014-11-26 11:18:05 -0600

    f: test tapestry-ioc style plugins

--- mrb-plugin-test/src/main/java/com/github/osndok/mrb/plugin/test/Main.java
+++ mrb-plugin-test/src/main/java/com/github/osndok/mrb/plugin/test/Main.java
@@ -1,6 +1,9 @@
 package com.github.osndok.mrb.plugin.test;
 
 import javax.module.Plugins;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
 
 /**
  * Created by robert on 11/10/14.
@@ -9,7 +12,7 @@ public
 class Main
 {
 	public static
-	void main(String[] args)
+	void main(String[] args) throws IOException
 	{
 		for (Quacker quacker : Plugins.load(Quacker.class))
 		{
@@ -24,6 +27,19 @@ class Main
 		{
 			System.err.println("Running: "+plugin);
 			plugin.run();
+		}
+
+		System.err.flush();
+
+		//This parallels the way that tapestry-ioc scans for it's modules...
+		//For a proper implementation, it must list all the manifest files for our immediate dependencies
+		//***AND*** everything in our startup context.
+		Enumeration<URL> urls = Main.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+
+		while (urls.hasMoreElements())
+		{
+			URL url = urls.nextElement();
+			System.out.println(url);
 		}
 	}
 }
