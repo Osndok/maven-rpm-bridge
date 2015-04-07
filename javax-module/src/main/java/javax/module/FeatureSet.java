7d6341d2ce5660e9c6fcb667a1ebe2e18c672484
Robert Hailey <rhailey@allogy.com>
2015-04-07 15:35:04 -0500

    F: FeatureSet class

---
+++ javax-module/src/main/java/javax/module/FeatureSet.java
@@ -0,0 +1,296 @@
+package javax.module;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * A FeatureSet is a utility class that conveys a set of string-representable requirements
+ * usually for determining processing adequacy. Furthermore, an easily human-readable and
+ * machine-parsable string representation is provided so that feature sets can be easily
+ * saved & restored.
+ *
+ * The "arguments" imply backwards compatibility, and the current implementation cannot
+ * represent or test "specific" version requirements. Therefore, if you have a requirement
+ * for "any java version 7 or above", then use "java(7)", but if you have a requirement
+ * that it "must be java 7" (e.g. not 8), then use "java7" (no parenthesis, 7 is in the key).
+ *
+ * Similarly, if you wish to apply this to a versioning scheme such as symver where the
+ * first version block indicates backwards compatibility, then you may have to insert the
+ * major version into the key (e.g. "symver4(4.3.2)") to get the desired effect.
+ *
+ * By convention, only lowercase keys are used in a featureset, as it *is* case sensitive.
+ *
+ * e.g. "java(7),maven(3.2),centos(6.5),gcc(4.7),rpmbuild(4.9),capillary-ppw(32.11)"
+ */
+public final
+class FeatureSet
+{
+	private final
+	Map<String, Version> versionRequirements = new LinkedHashMap<>();
+
+	public
+	FeatureSet add(String key)
+	{
+		return add(key, (Version) null);
+	}
+
+	public
+	FeatureSet add(String key, String version)
+	{
+		if (version == null)
+		{
+			return add(key, (Version) null);
+		}
+		else
+		{
+			return add(key, new Version(version));
+		}
+	}
+
+	public
+	FeatureSet add(String key, Version version)
+	{
+		mustNotContainAnyCommasOrParanthesis("key", key);
+
+		if (version != null)
+		{
+			mustNotContainAnyCommasOrParanthesis("version", version.toString());
+		}
+
+		_add(key, version);
+
+		toStringCache=null;
+		return this;
+	}
+
+	private
+	void mustNotContainAnyCommasOrParanthesis(String identifier, String value)
+	{
+		throwIfFound(',', "commas", identifier, value);
+		throwIfFound('(', "parenthesis", identifier, value);
+		throwIfFound(')', "parenthesis", identifier, value);
+	}
+
+	private
+	void _add(String key, Version version)
+	{
+		Version existing = versionRequirements.get(key);
+
+		if (existing == null)
+		{
+			//NB: version could be null too
+			versionRequirements.put(key, version);
+		}
+		else
+		if (version!=null && existing.isOlderThan(version))
+		{
+			versionRequirements.put(key, version);
+		}
+	}
+
+	private
+	void throwIfFound(char c, String commas, String identifier, String value)
+	{
+		final
+		int position=value.indexOf(c);
+
+		if (position>=0)
+		{
+			throw new IllegalArgumentException(identifier+" may not contain any "+commas+": '"+value+"' @ "+position);
+		}
+	}
+
+	public
+	FeatureSet addAll(FeatureSet other)
+	{
+		for (Map.Entry<String, Version> me : other.versionRequirements.entrySet())
+		{
+			String key=me.getKey();
+			Version version=me.getValue();
+			_add(key, version);
+		}
+
+		return this;
+	}
+
+	public
+	boolean containsKey(String key)
+	{
+		return versionRequirements.containsKey(key);
+	}
+
+	public
+	boolean providesAtLeast(String key, String version)
+	{
+		if (version==null)
+		{
+			return providesAtLeast(key, (Version)null);
+		}
+		else
+		{
+			return providesAtLeast(key, new Version(version));
+		}
+	}
+
+	public
+	boolean providesAtLeast(String key, Version minimum)
+	{
+		if (minimum==null)
+		{
+			return versionRequirements.containsKey(key);
+		}
+
+		Version provision=versionRequirements.get(key);
+
+		return (provision != null && !provision.isOlderThan(minimum));
+	}
+
+	private transient
+	String toStringCache;
+
+	@Override
+	public
+	String toString()
+	{
+		if (toStringCache==null)
+		{
+			final
+			StringBuilder sb = new StringBuilder();
+
+			for (Map.Entry<String, Version> me : versionRequirements.entrySet())
+			{
+				String key=me.getKey();
+				Version version=me.getValue();
+
+				if (sb.length()!=0)
+				{
+					sb.append(',');
+				}
+
+				sb.append(key);
+
+				if (version!=null)
+				{
+					sb.append('(');
+					sb.append(version);
+					sb.append(')');
+				}
+			}
+
+			toStringCache=sb.toString();
+		}
+
+		return toStringCache;
+	}
+
+	public static
+	FeatureSet fromString(String s)
+	{
+		final
+		FeatureSet retval=new FeatureSet();
+
+		final
+		String[] bits=s.split(",");
+
+		for (String bit : bits)
+		{
+			final
+			int leftParanthesis=bit.indexOf('(');
+
+			if  (leftParanthesis==0)
+			{
+				throw new IllegalArgumentException("not a valid feature set string: '"+bit+"' / '"+s+"'");
+			}
+			else
+			if (leftParanthesis<0)
+			{
+				retval.add(bit, (Version)null);
+			}
+			else
+			if (bit.charAt(bit.length()-1)==')')
+			{
+				retval.add(bit.substring(0, leftParanthesis), bit.substring(leftParanthesis+1, bit.length()-1));
+			}
+			else
+			{
+				throw new IllegalArgumentException("not a valid feature set string: '"+bit+"' / '"+s+"'");
+			}
+		}
+
+		return retval;
+	}
+
+	/**
+	 * @param smaller
+	 * @return true if (and only if) this feature set encompasses all the versions of the given smaller set
+	 */
+	public
+	boolean isSuperSetOf(FeatureSet smaller)
+	{
+		for (Map.Entry<String, Version> me : smaller.versionRequirements.entrySet())
+		{
+			String key=me.getKey();
+			Version version=me.getValue();
+
+			if (version==null)
+			{
+				if (!this.versionRequirements.containsKey(key))
+				{
+					return false;
+				}
+			}
+			else
+			{
+				Version shouldBeNewer=this.versionRequirements.get(key);
+
+				if (shouldBeNewer==null || shouldBeNewer.isOlderThan(version))
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public
+	boolean isSubSetOf(FeatureSet larger)
+	{
+		for (Map.Entry<String, Version> me : this.versionRequirements.entrySet())
+		{
+			String key=me.getKey();
+			Version version=me.getValue();
+
+			if (version==null)
+			{
+				if (!larger.versionRequirements.containsKey(key))
+				{
+					return false;
+				}
+			}
+			else
+			{
+				Version shouldBeNewer=larger.versionRequirements.get(key);
+
+				if (shouldBeNewer==null || shouldBeNewer.isOlderThan(version))
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public
+	boolean satisfies(FeatureSet smaller)
+	{
+		return this.isSuperSetOf(smaller);
+	}
+
+	public
+	boolean isSatisfiedBy(FeatureSet larger)
+	{
+		return this.isSubSetOf(larger);
+	}
+}
---
+++ javax-module/src/test/java/javax/module/FeatureSetTest.java
@@ -0,0 +1,113 @@
+package javax.module;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+/**
+ * Created by robert on 4/7/15.
+ */
+@Test
+public
+class FeatureSetTest extends Assert
+{
+	@Test
+	public
+	void testUnaryOperations()
+	{
+		final
+		FeatureSet fs=new FeatureSet();
+
+		assertFalse(fs.containsKey("java"));
+
+		fs.add("java", "7");
+
+		assertTrue(fs.containsKey("java"));
+		assertTrue(fs.providesAtLeast("java", "7"));
+		assertTrue(fs.providesAtLeast("java", "7.0"));
+		assertTrue(fs.providesAtLeast("java", "6.99"));
+		assertTrue(fs.providesAtLeast("java", "1"));
+		assertTrue(fs.providesAtLeast("java", (Version)null));
+		assertTrue(fs.providesAtLeast("java", (String)null));
+
+		assertFalse(fs.providesAtLeast("blather", (Version)null));
+		assertFalse(fs.providesAtLeast("blather", "1.2.3"));
+		assertFalse(fs.providesAtLeast("java", "100"));
+		assertFalse(fs.providesAtLeast("java", "10"));
+		assertFalse(fs.providesAtLeast("java", "8"));
+		assertFalse(fs.providesAtLeast("java", "7.1"));
+		assertFalse(fs.providesAtLeast("java", "7.0.0.0.1"));
+
+		fs.add("java", "8");
+		assertTrue(fs.providesAtLeast("java", "8"));
+	}
+
+	@Test
+	public
+	void testToString()
+	{
+		assertEquals(fs().add("java"     )                    .toString(), "java"              );
+		assertEquals(fs().add("java"     ).add("java", "7")   .toString(), "java(7)"           );
+		assertEquals(fs().add("java", "7").add("java", "6")   .toString(), "java(7)"           );
+		assertEquals(fs().add("java", "6").add("java", "7")   .toString(), "java(7)"           );
+		assertEquals(fs().add("java", "7")                    .toString(), "java(7)"           );
+		assertEquals(fs().add("java"     ).add("blather"     ).toString(), "java,blather"      );
+		assertEquals(fs().add("java", "7").add("blather"     ).toString(), "java(7),blather"   );
+		assertEquals(fs().add("java"     ).add("blather", "1").toString(), "java,blather(1)"   );
+		assertEquals(fs().add("java", "7").add("blather", "1").toString(), "java(7),blather(1)");
+	}
+
+	private
+	FeatureSet fs()
+	{
+		return new FeatureSet();
+	}
+
+	@Test
+	public
+	void testParserAndComparator()
+	{
+		final
+		FeatureSet linux=FeatureSet.fromString("java(7),maven(3.2),rhel,centos(6.5),gcc(4.7),rpmbuild(4.9),capillary-ppw(32.11),linux,unix");
+
+		final
+		FeatureSet windows=FeatureSet.fromString("java(8),maven(3),microsoft,c#,visualbasic,capillary-streamer(12.34)");
+
+		assertTrue(linux.containsKey("java"));
+		assertTrue(linux.containsKey("maven"));
+		assertTrue(linux.containsKey("rhel"));
+		assertTrue(linux.containsKey("unix"));
+		assertTrue(linux.providesAtLeast("java", "7"));
+		assertTrue(linux.providesAtLeast("java", "7.0"));
+		assertTrue(linux.providesAtLeast("java", "6"));
+		assertTrue(linux.providesAtLeast("java", "6.999"));
+		assertTrue(linux.providesAtLeast("rpmbuild", (Version)null));
+
+		assertTrue(windows.containsKey("c#"));
+
+		final
+		FeatureSet buildableOnLinux=FeatureSet.fromString("java(6),maven,rhel,rpmbuild(3)");
+
+		assertTrue(linux.satisfies(buildableOnLinux));
+		assertTrue(buildableOnLinux.isSatisfiedBy(linux));
+		assertFalse(buildableOnLinux.satisfies(linux));
+		assertFalse(linux.isSatisfiedBy(buildableOnLinux));
+		assertFalse(windows.satisfies(buildableOnLinux));
+		assertFalse(buildableOnLinux.isSatisfiedBy(windows));
+		assertFalse(buildableOnLinux.satisfies(windows));
+		assertFalse(windows.isSatisfiedBy(buildableOnLinux));
+
+		final
+		FeatureSet buildableOnWindows=FeatureSet.fromString("java(8.0),microsoft,c#");
+
+		assertTrue(windows.satisfies(buildableOnWindows));
+		assertTrue(buildableOnWindows.isSatisfiedBy(windows));
+		assertFalse(buildableOnWindows.satisfies(windows));
+		assertFalse(windows.isSatisfiedBy(buildableOnWindows));
+		assertFalse(linux.satisfies(buildableOnWindows));
+		assertFalse(buildableOnWindows.isSatisfiedBy(linux));
+		assertFalse(buildableOnWindows.satisfies(linux));
+		assertFalse(linux.isSatisfiedBy(buildableOnWindows));
+	}
+}
