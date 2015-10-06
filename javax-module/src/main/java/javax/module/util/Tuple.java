a1ead21a210dcb5faad9257d62bd250b6c7ee06c
Robert Hailey <rhailey@allogy.com>
2015-10-06 13:02:50 -0500

    F: Tuple.create -> Tuple.of

--- javax-module/src/main/java/javax/module/util/Tuple.java
+++ javax-module/src/main/java/javax/module/util/Tuple.java
@@ -8,6 +8,13 @@ package javax.module.util;
  * A particular design decision of note is that these Tuples do *NOT* inherit the class of lower
  * cardinality (as I have seen many Tuple classes do on github). IMO it is both incorrect and
  * not meaningful to claim that "PlayingCard isa Tuple2<Suit,Number>" if "Tuple2 isa Tuple1".
+ *
+ * NB: at one time, I considered making Tuple extend AbstractCollection, but decided that it
+ * would generate more confusion that utility; particularly if one of the tuple elements was
+ * a collection or named 'size'... which is quite plausible. Less confusing *might* be to
+ * simply implement Iterable... which might still cause a bit of confusion (if an iterator
+ * or collection is one of the elements) for marginal utility (being able to operate on tuples
+ * of any cardinality) for which there is presently no known use case (please show me!).
  */
 public
 class Tuple
@@ -25,19 +32,19 @@ class Tuple
 	}
 
 	public static <X, Y>
-	Tuple2<X, Y> create(X first, Y second)
+	Tuple2<X, Y> of(X first, Y second)
 	{
 		return new Tuple2<X, Y>(first, second);
 	}
 
 	public static <X, Y, Z>
-	Tuple3<X, Y, Z> create(X first, Y second, Z third)
+	Tuple3<X, Y, Z> of(X first, Y second, Z third)
 	{
 		return new Tuple3<X, Y, Z>(first, second, third);
 	}
 
 	public static <X, Y, Z, W>
-	Tuple4<X, Y, Z, W> create(X first, Y second, Z third, W fourth)
+	Tuple4<X, Y, Z, W> of(X first, Y second, Z third, W fourth)
 	{
 		return new Tuple4<X, Y, Z, W>(first, second, third, fourth);
 	}
